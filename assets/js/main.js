969e53a2f1cda5e9a304663151847284a29ce5f1
dannymossey <dmosseywebdev@gmail.com>
2021-04-08 14:58:51 +0100

    my first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const burger = document.getElementById('burger');
+
+const navBar = document.getElementsByTagName('ul')[0];
+
+burger.addEventListener('click', () => {
+
+  
+  navBar.classList.toggle('show');
+  
+
+});
+
+
+
+
+
